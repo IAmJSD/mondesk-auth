38bc31dda3920afb55462ed6d6a1d4e12cbc97a8
Jake Gealer <jake@gealer.email>
2019-12-20 10:20:34 +0000

    Is it function order?

--- handler.js
+++ handler.js
@@ -26,8 +26,8 @@ if (params.get("code") && params.get("state")) {
     xhr.onload = function() {
         if (xhr.status === 200) {
             const json = JSON.parse(xhr.responseText);
-            copyToClipboard(json.access_token);
             document.body.innerHTML = "<h1 class=\"title is-1\">API Token</h1>\n<p>Your API token has been copied to your clipboard. Make sure to accept the notification on your phone!</p>";
+            copyToClipboard(json.access_token);
             localStorage.removeItem("clientId");
             localStorage.removeItem("clientSecret");
             localStorage.removeItem("state");
